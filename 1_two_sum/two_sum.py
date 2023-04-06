8e9424beae1c4197c1f59931cc25bfb521b2c4f9
garya <tcgaryau@gmail.com>
2023-04-06 16:12:55 -0700

    Added a brute force and binary search solution to 3sum

---
+++ 1_two_sum/two_sum.py
@@ -0,0 +1,65 @@
+from typing import List
+
+
+class Solution:
+    """
+    Brute Force
+    Loops through the list twice and see if they sum up to the target
+    Time Complexity O(n^2)
+    Space Complexity O(1)
+    """
+
+    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    """
+    Two Pointers with sort
+    Sort the nums in an ascending order. Then we can use two-pointers where we increment left if nums[left] + nums[right] < target and vice versa until it equals the solution
+    Time Complexity O(nlogn) - this is mainly determined by the python's sorting algorithm which uses TimSort
+    Space Complexity O(n) as we will have to store a hashmap to lookup the values to the index
+    """
+
+    def twoSumTwoPointer(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {val: ind for ind, val in enumerate(nums)}
+        nums.sort()
+        left_ptr, right_ptr = 0, len(nums) - 1
+        while left_ptr < right_ptr:
+            if nums[left_ptr] + nums[right_ptr] == target:
+                break
+            if nums[left_ptr] + nums[right_ptr] < target:
+                left_ptr += 1
+            else:
+                right_ptr -= 1
+        return [nums_map[nums[left_ptr]], nums_map[nums[right_ptr]]]
+
+    """
+    Hash Map
+    We add a hash map to track a value to the index. Then we loop through nums and if it exists in the hash map, we can return that current index and the hash map's stored index
+    Time Complexity O(n)
+    Space Complexity O(n)
+    """
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for ind, val in enumerate(nums):
+            find_num = target - val
+            if find_num in map:
+                return [map[find_num], ind]
+            map[val] = ind
+
+
+def main():
+    sol = Solution()
+    test_1 = {"nums": [2, 7, 11, 15], "target": 9}
+    test_2 = {"nums": [3, 2, 4], "target": 6}
+    test_3 = {"nums": [3, 3], "target": 6}
+    print(sol.twoSumTwoPointer(**test_1))
+    print(sol.twoSumTwoPointer(**test_2))
+    print(sol.twoSumTwoPointer(**test_3))
+
+
+if __name__ == "__main__":
+    main()
--- Medium/15_3Sum/three_sum.py
+++ Medium/15_3Sum/three_sum.py
@@ -2,6 +2,61 @@ from typing import List
 
 
 class Solution:
+
+    """
+    Brute force approach using 3 pointers to iterate through the list. It sorts the nums list first.
+    Time Complexity: O(n^3)
+    Space Complexity: O(1)
+    """
+
+    def threeSumBruteForce(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[k] == nums[k-1]:
+                        continue
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        res.append([nums[i], nums[j], nums[k]])
+        return res
+
+    """
+    Make 2 pointers that iterate it from i to N-1 and j=i+1 to N-1 and iterate through it. The last value we can use a binary search on the remaining subtree for - (nums[i] + nums[j])
+    and if it exists, we can return it.
+    Time Complexity: O(n^2 log(n))
+    S
+    """
+
+    def threeSumBinarySearch(self, nums: List[int]) -> List[List[int]]:
+        def binarySearch(low, high, target, nums) -> bool:
+            if low > high:
+                return False
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] > target:
+                return binarySearch(low, mid - 1, target, nums)
+            else:
+                return binarySearch(mid + 1, high, target, nums)
+
+        nums.sort()
+        res = []
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                target = -(nums[i] + nums[j])
+                if binarySearch(j+1, len(nums) - 1, target, nums):
+                    res.append([nums[i], nums[j], target])
+        return res
+
     """
     Sort the nums first. Then we loop through nums and then put in a 2-pointer solution from two-sum to determine if we need to add it to our results.
     Time Complexity: O(n^2) - because sorting is O(nlogn) and looping through it twice is O(n^2)
@@ -33,11 +88,11 @@ class Solution:
 
 def main():
     sol = Solution()
-    test_1 = [-1, 0, 1, 2, -1, -4]
-    test_2 = [0, 1, 1]
-    test_3 = [0, 0, 0]
+    test_1 = [-1, 0, 1, 2, -1, -4]  # [[-1, -1, 2], [-1, 0, 1]]
+    test_2 = [0, 1, 1]  # []
+    test_3 = [0, 0, 0]  # [[0,0,0]]
     for test in [test_1, test_2, test_3]:
-        print(sol.threeSum(test))
+        print(sol.threeSumBinarySearch(test))
 
 
 if __name__ == "__main__":
